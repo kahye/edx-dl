3c708e60eaccf20a7264425338d9618eda23d4f6
Seyeon Weon <sweon@users.noreply.github.com>
2015-05-24 23:37:12 +0900

    A bash script to download multiple courses.
    Format of the input file:
    
    directory for course 1
    URL for course 1
    directory for course 2
    URL for course 2
    #comment line or directory for an old course
    #comment line or URL for an old course

---
+++ edx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IFS=''
+counter=0
+while read line
+do
+	case "$line" in \#*) continue ;; esac
+    if [ $counter == 0 ]; then
+		cd $line
+		let counter=counter+1
+	else
+		edx-dl -u edx_username -p password
+		let counter=0
+		cd ..
+	fi
