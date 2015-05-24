ade50d49c54603b88d67a53f9103123ebaadcfb3
Seyeon Weon <sweon@users.noreply.github.com>
2015-05-25 08:56:49 +0900

    Skip empty lines.

--- edx.sh
+++ edx.sh
@@ -3,7 +3,10 @@ IFS=''
 counter=0
 while read line
 do
-	case "$line" in \#*) continue ;; esac
+    case "$line" in
+        "") continue ;;
+        \#*) continue ;;
+    esac
     if [ $counter == 0 ]; then
 		cd $line
 		let counter=counter+1
@@ -13,4 +16,3 @@ do
 		cd ..
 	fi
 done <$1
-
